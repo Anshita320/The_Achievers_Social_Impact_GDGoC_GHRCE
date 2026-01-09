c6b4fef11ea7f2fc76052420547e94712c39dfc5
Anshita320 <anshitabante4@gmail.com>
2026-01-09 19:56:41 +0530

    eslint config cleanup and safe rule tweaks

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,7 +5,9 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  {
+    ignores: ['dist'],
+  },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -17,18 +19,30 @@ export default [
         sourceType: 'module',
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: {
+      react: {
+        version: '18.3',
+      },
+    },
     plugins: {
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      /* Base Recommended */
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+
+      /* Custom Safe Rules */
       'react/jsx-no-target-blank': 'off',
+      'react/prop-types': 'off',
+
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
