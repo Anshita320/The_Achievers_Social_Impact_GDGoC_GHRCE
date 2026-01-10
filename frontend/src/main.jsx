cd6ae3266b3254f7b18469a1f4e7162d97a55429
Anshita320 <anshitabante4@gmail.com>
2026-01-10 12:31:02 +0530

    main.jsx cleanup and router import fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,32 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, Routes, Route } from "react-router";
-import CreateTrip from "./create-trip/index";
+import CreateTrip from "./create-trip";
 import Header from "./components/custom/Header";
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import Viewtrip from "./view-trip/[tripId]/index";
+import Viewtrip from "./view-trip/[tripId]";
 import Footer from "./view-trip/components/Footer";
 import MyTrips from "./my-trips";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
+      <GoogleOAuthProvider
+        clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}
+      >
         <Header />
+
         <Routes>
-          <Route path="/" element={<App />}></Route>
-          <Route path="/create-trip" element={<CreateTrip />}></Route>
-          <Route path="/view-trip/:tripId" element={<Viewtrip/>}></Route>
-          <Route path="/my-trips" element={<MyTrips/>}></Route>
+          <Route path="/" element={<App />} />
+          <Route path="/create-trip" element={<CreateTrip />} />
+          <Route path="/view-trip/:tripId" element={<Viewtrip />} />
+          <Route path="/my-trips" element={<MyTrips />} />
         </Routes>
-        <Footer/>
+
+        <Footer />
       </GoogleOAuthProvider>
     </BrowserRouter>
   </StrictMode>
